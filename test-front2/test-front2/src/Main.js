1678156c2b0f76c815fb2d8a80178f82f1e9bc15
Alejo <vera.alejo0903@gmail.com>
2021-05-17 10:32:15 -0300

    Icons on columns

--- test-front2/test-front2/src/Main.js
+++ test-front2/test-front2/src/Main.js
@@ -1,27 +1,43 @@
 import React, { useState } from "react";
 import MUIDataTable from "mui-datatables";
 
+import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
+import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
+import CodeIcon from '@material-ui/icons/Code';
+import CloudIcon from '@material-ui/icons/Cloud';
 
-
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
+// import InputLabel from "@material-ui/core/InputLabel";
+// import MenuItem from "@material-ui/core/MenuItem";
+// import FormHelperText from "@material-ui/core/FormHelperText";
+// import FormControl from "@material-ui/core/FormControl";
+// import Select from "@material-ui/core/Select";
 
 function Main() {
-  const [responsive, setResponsive] = useState("vertical");
-  const [tableBodyHeight, setTableBodyHeight] = useState("400px");
-  const [tableBodyMaxHeight, setTableBodyMaxHeight] = useState("");
+  // const [responsive, setResponsive] = useState("vertical");
+  // const [tableBodyHeight, setTableBodyHeight] = useState("400px");
+  // const [tableBodyMaxHeight, setTableBodyMaxHeight] = useState("");
 
-  const columns = ["Type","Name", "Sensor", "Date", "Time", "Status", "Action"];
+  const columns = [
+    {
+      name:"Type",
+      options: {
+        customBodyRenderLite: (dataIndex) => {
+          return (
+            <EmojiObjectsIcon />
+          )
+        }
+      }
+    }
+    ,"Name", "Sensor", "Date", "Time", "Status", "Action"
+  ];
 
   const options = {
     filter: true,
     filterType: "dropdown",
-    responsive,
-    tableBodyHeight,
-    tableBodyMaxHeight
+    viewColumns: false,
+    // responsive,
+    // tableBodyHeight,
+    // tableBodyMaxHeight
   };
 
   const data = [
@@ -34,17 +50,27 @@ function Main() {
     ["", "Alert Name", "Sensor Name", "12/01/2021", "03:21 PM", "Active", ""],
     ["", "Alert Name", "Sensor Name", "12/01/2021", "03:21 PM", "Active", ""],
     ["", "Alert Name", "Sensor Name", "12/01/2021", "03:21 PM", "Active", ""],
-    
     ];
+
+    const components = {
+      icons: {
+        EmojiObjectsIcon,
+        MeetingRoomIcon,
+        CodeIcon,
+        CloudIcon
+      }
+    }
 
   return (
     <React.Fragment>
       <MUIDataTable
         className="datatable"
         title={"ALERTS"}
-        data={data}
-        columns={columns}
-        options={options}
+        {...{data, columns, options, components}}
+        // data={data}
+        // columns={columns}
+        // options={options}
+        // components={components}
       />
     </React.Fragment>
   );
